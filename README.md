16cbdda9b1b621b10c3a6f3472bb4f5abba7ed84
neil patel <neil@myindianweddingapp.com>
2014-05-08 20:11:15 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+== README
+
+Prototype rewards app for freelancers 
+
+
+* Ruby 2.0
+
+* Rails 4.0.4
+
+* Bootstrap 3.0
+
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myrewardsapp::Application.config.secret_key_base = 'f663caf89b9066254179b9c90b2ae95ae0b6cb625fdd130de226e72cca2e9572401b11a57a72f4f7b0c09f413542f3e53a0c4381c18ea9b9b9c30e7e06f9e7ee'
